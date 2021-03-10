39487230b3908e660d4b480fdcfcc04d8f33ed6e
Admin <Admin@DESKTOP-PEEPDTL.home>
2021-03-10 19:08:13 +0100

    First commit, commit number 1

---
+++ exampleLoop/src/exampleLoop/Loop.java
@@ -0,0 +1,19 @@
+package exampleLoop;
+
+public class Loop {
+	
+	public static void main(String[] args) {
+		
+		// Hola buenas tardes
+
+	int number = 1;
+	
+	while (number < 56) {
+		
+		System.out.println("I love java so many times: " + number);
+		number++;
+		}
+	
+	}
+
+}
